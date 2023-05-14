9fc3c7c5fa66b20c969a597814f11adfde7c840f
Ousman <usman.traid@gmail.com>
2023-05-14 08:14:24 -0400

    all tasks completed

---
+++ 0x00-pagination/tt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def test():
+    # assert(type(page) == int and page > 0 and page_size > 0)
+    a = [1,2, 3, 4, 5]
+    return a
+    # print(a[1:3])
+
+# print(test()[1:3])
+for i in range(2):
+    print(i)
+
